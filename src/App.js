3832e7f0110f0784f0ae178d26e5873dda25172e
ajinkya-kalane <ajinkyakalane7@gmail.com>
2023-01-21 20:19:06 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Click to Login!</h1>
+        <button className="LoginClick">Login</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
